24f7c5a57885a21e3208500e6ac1661d70f2b103
ArjunMS <arjunms2005@gmail.com>
2026-02-03 21:05:24 +0530

    ppid and vmlinux work

--- edr-agent-ebpf/src/main.rs
+++ edr-agent-ebpf/src/main.rs
@@ -1,11 +1,20 @@
 #![no_std]
 #![no_main]
 
+// THE MAGIC MODULE IS HERE
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+mod vmlinux;
+
+use vmlinux::task_struct;
+
 use aya_ebpf::{
     macros::{map, tracepoint},
     programs::TracePointContext,
     maps::PerfEventArray,
-    helpers::{bpf_get_current_pid_tgid, bpf_get_current_comm},
+    helpers::{bpf_get_current_pid_tgid, bpf_get_current_comm, bpf_get_current_task_btf},
 };
 use edr_agent_common::ProcessEvent;
 
@@ -21,25 +30,33 @@ pub fn edr_agent(ctx: TracePointContext) -> u32 {
 }
 
 fn try_edr_agent(ctx: TracePointContext) -> Result<u32, u32> {
-    // 1. Get the PID
     let pid = (bpf_get_current_pid_tgid() >> 32) as u32;
-    
-    // 2. Get the Command Name
     let comm = bpf_get_current_comm().unwrap_or([0; 16]);
 
-    // 3. Mock the PPID (Safety Mechanism)
-    // Since reading task_struct is failing on this VM, we default to 1 (init)
-    // This allows the pipeline to function for the demo.
-    let ppid = 1; 
+    // 1. Get the Task Struct
+    let task = unsafe { bpf_get_current_task_btf() as *const task_struct };
+    
+    // 2. Read the Real Parent PID (No Mocking!)
+    let ppid = unsafe {
+        if !task.is_null() {
+            let parent = (*task).real_parent;
+            if !parent.is_null() {
+                (*parent).tgid as u32
+            } else {
+                0
+            }
+        } else {
+            0
+        }
+    };
 
     let event = ProcessEvent {
         pid: pid,
-        ppid: ppid, 
+        ppid: ppid, // 100% Real Data
         cmd: comm,
     };
 
     EVENTS.output(&ctx, &event, 0);
-
     Ok(0)
 }
 
